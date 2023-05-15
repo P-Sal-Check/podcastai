96627c3b76c77130de0e3c0cdba871c95773d39d
sunio <sunio@pubg.com>
2023-05-15 20:54:17 +0900

    feat: 앱에서 NewsScrapper를 사용하여 랜덤성을 가지는 데이터 노출하도록 변경

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from Utils.NewsScrapper import NewsScrapper;
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-test_news_headline_input = "이르면 16일 전기료 인상, ‘냉방비 폭탄’ 여부 날씨에 달렸다… 외식업·상가발 물가상승 불가피";
+news_route_url = "https://news.naver.com/main/ranking/popularDay.naver";
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         print('request POST started.');
-        news_headline = test_news_headline_input;
+        news_headline = NewsScrapper(news_route_url).get_news(1)[0].title;
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(news_headline),
@@ -26,9 +26,6 @@ def index():
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
 def generate_prompt(news_headline):
     return """Suggest a podcast script about '{}' as following form sheet\n"
