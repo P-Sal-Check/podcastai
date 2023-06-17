d9d5655dbe2f9cce6ef9303c27119639d6f4289d
sunio <sunio@pubg.com>
2023-06-18 00:09:56 +0900

    feat: YoutubeUploader 로직 작성
    - google ㅐ인증

--- Utils/YoutubeUploader.py
+++ Utils/YoutubeUploader.py
@@ -1,3 +1,52 @@
-class YoutubeUploader:
+# https://github.com/P-Sal-Check/auto-podcast-ai/blob/master/youtube_module.py
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from google.oauth2.service_account import Credentials
+
+
+class YouTubeUploader:
     def __init__(self):
-        pass
+
+        # Use the client_secrets file downloaded from your project
+        self.client_secrets_file = "client_secrets.json"
+
+        # Use the YouTube Data API v3
+        self.api_service_name = "youtube"
+        self.api_version = "v3"
+
+        self.youtube = None
+
+    def __get_authenticated_service(self):
+        # Get credentials and create an API client
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            self.client_secrets_file, ["https://www.googleapis.com/auth/youtube.upload"])
+        credentials = flow.run_local_server()
+        credentials = Credentials.from_service_account_file(
+            self.client_secrets_file, scopes=[
+                "https://www.googleapis.com/auth/youtube.upload"]
+        )
+        youtube = googleapiclient.discovery.build(
+            self.api_service_name, self.api_version, credentials=credentials)
+        return youtube
+
+    def upload_video(self, title, description):
+        self.youtube = self.__get_authenticated_service()
+
+        request = self.youtube.videos().insert(
+            part="snippet,status",
+            body={
+                "snippet": {
+                    "categoryId": '25',
+                    "description": description,
+                    "title": title
+                },
+                "status": {
+                    "privacyStatus": "private"
+                }
+            },
+            media_body=googleapiclient.http.MediaFileUpload(f'videos/${title}')
+        )
+        response = request.execute()
+        return response
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, render_template, request, url_for
 from Utils.NewsScrapper import NewsScrapper
 from Utils.SpeechGenerator import SpeechGenerator
 from Utils.VideoGenerator.ThumbVideoGenerator import ThumbVideoGenerator
+from Utils.YoutubeUploader import YouTubeUploader
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,7 +17,9 @@ def index():
         print('Apppy - request POST started.')
         news = NewsScrapper(news_route_url).get_news(1)[0]
         news_headline = news.title
+        news_description = news.text
         news_top_image = news.top_image
+
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(news_headline),
@@ -25,9 +28,13 @@ def index():
             max_tokens=256,
             stop=None,
         )
+
         ai_script = response.choices[0].text
         audio = SpeechGenerator().generate(ai_script)
+        if(news_top_image == ''):
+            pass
         ThumbVideoGenerator().generate(news_top_image, audio, news_headline)
+        YouTubeUploader().upload_video(news_headline, news_description)
         return redirect(url_for("index", result=ai_script))
 
     result = request.args.get("result")
